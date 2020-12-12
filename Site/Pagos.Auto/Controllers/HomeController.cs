a76ee73cd0c3ce05ae024f9aff1d3ed29ac21c3c
Alexis Alulema <alexis.alulema@gmail.com>
2020-12-12 00:01:54 -0500

    Small improvements in totals

--- Site/Pagos.Auto/Controllers/HomeController.cs
+++ Site/Pagos.Auto/Controllers/HomeController.cs
@@ -2,13 +2,22 @@
 using Microsoft.Extensions.Logging;
 using Pagos.Auto.Models;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 using Pagos.Auto.Data;
 
 namespace Pagos.Auto.Controllers
 {
     public class HomeController : Controller
     {
+        public class ButtonModel
+        {
+            public string content { get; set; }
+            public string cssClass { get; set; }
+        }
+
+        private decimal Total = 120 * 60;
         private readonly ILogger<HomeController> _logger;
         private readonly IMainData _mainData;
 
@@ -21,7 +30,15 @@ namespace Pagos.Auto.Controllers
         public async Task<IActionResult> Index()
         {
             var data = await _mainData.GetAllCarPaymentsAsync();
+            var totalPayed = data.Sum(x => x.Amount);
+            var totalToPay = Total - totalPayed;
+
             ViewBag.DataSource = data;
+            ViewBag.DialogButtonsOk = new ButtonModel() { cssClass = "e-primary", content = "Guardar" };
+            ViewBag.DialogButtonsCancel = new ButtonModel() { content = "Cancelar", cssClass = "e-flat" };
+            ViewBag.TotalPayed = totalPayed;
+            ViewBag.TotalToPay = totalToPay;
+
             return View();
         }
 
--- Site/Pagos.Auto/wwwroot/js/site.js
+++ Site/Pagos.Auto/wwwroot/js/site.js
@@ -1,4 +1,23 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var opencount = -1;
 
-// Write your JavaScript code.
+window.onload = function () {
+    document.getElementById('primarybtn').onclick = function () {
+        opencount++;
+        var dialog = document.getElementById("dialog").ej2_instances[0];
+        dialog.show();
+    };
+}
+
+function onOverlayClick() {
+    var dialog = document.getElementById("dialog").ej2_instances[0];
+    dialog.hide();
+}
+
+function validation(args) {
+    args.cancel = opencount < 0;
+}
+
+function dlgButtonClick() {
+    var dialogObj = document.getElementById('dialog').ej2_instances[0];
+    dialogObj.hide();
+}
